b762e9045ef519754ee1c2b0056a4efe8af2ede8
compwron <wordymusic@gmail.com>
2013-08-09 18:20:59 -0500

    displaying with simple print-and-clear

--- lib/display.rb
+++ lib/display.rb
@@ -3,13 +3,15 @@ class Display
     @game = game
   end
 
-  def evolve(evolutions)
-
-  end
-
   def display
     @game.grids.each { |grid|
-
+      clear_screen
+      puts grid
+      sleep 0.5
     }
+  end
+
+  def clear_screen
+    print "\e[2J\e[f"
   end
 end
