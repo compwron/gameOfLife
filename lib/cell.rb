dd27ee67a9a2b00b277420bc4b14e047a4b01edc
compwron <wordymusic@gmail.com>
2013-08-09 07:33:02 -0500

    neighbor working correctly, Grid test still failing

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,5 +1,6 @@
 class Cell
   attr_reader :x, :y
+
   def initialize(x, y, state)
     @x = x
     @y = y
@@ -20,9 +21,8 @@ class Cell
 
   def is_neighbor_of?(cell)
     neighbor_permutations = [-1, 1, 0].permutation(2).to_a + [[1, 1] + [-1, -1]]
-    neighbor_permutations.each{|x_addition, y_addition|
-      return true if (@x == cell.x + x_addition &&
-      @y == cell.y + y_addition)
+    neighbor_permutations.each { |x_addition, y_addition|
+      return true if (@x + x_addition == cell.x && @y + y_addition == cell.y)
     }
     false
   end
