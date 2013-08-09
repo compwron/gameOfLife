53ef797d60314ff948401a02ec1d5bb5e610547e
compwron <wordymusic@gmail.com>
2013-08-09 07:59:08 -0500

    fixing neighbor_permutations data structure

--- lib/cell.rb
+++ lib/cell.rb
@@ -20,7 +20,7 @@ class Cell
   end
 
   def is_neighbor_of?(cell)
-    neighbor_permutations = [-1, 1, 0].permutation(2).to_a + [[1, 1] + [-1, -1]]
+    neighbor_permutations = [-1, 1, 0].permutation(2).to_a + [[1, 1]] + [[-1, -1]]
     neighbor_permutations.each { |x_addition, y_addition|
       return true if (@x + x_addition == cell.x && @y + y_addition == cell.y)
     }
