7831649d4a312047c5dab938919049f6697965fc
compwron <wordymusic@gmail.com>
2013-08-09 07:25:32 -0500

    WIp two failures in cell neighbor identification

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -4,22 +4,22 @@ describe Cell do
 
   it "cell is not valid neighbor of itself" do
     cell = Cell.new(0, 0, State.live)
-    cell.is_neighbor_of?(new Cell(0, 0, State.live)).should_be false
+    cell.is_neighbor_of?(Cell.new(0, 0, State.live)).should be_false
   end
 
   it "cell is valid neighbor of next on x axis" do
     cell = Cell.new(0, 0, State.live)
-    cell.is_neighbor_of?(new Cell(0, 1, State.live)).should_be true
+    cell.is_neighbor_of?(Cell.new(0, 1, State.live)).should be_true
   end
 
   it "cell is valid neighbor of next on y axis" do
     cell = Cell.new(0, 0, State.live)
-    cell.is_neighbor_of?(new Cell(1, 0, State.live)).should_be true
+    cell.is_neighbor_of?(Cell.new(1, 0, State.live)).should be_true
   end
 
   it "cell is valid neighbor of diagonal" do
     cell = Cell.new(0, 0, State.live)
-    cell.is_neighbor_of?(new Cell(1, 1, State.live)).should_be true
+    cell.is_neighbor_of?(Cell.new(1, 1, State.live)).should be_true
   end
 
 end
